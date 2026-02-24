c615481998e55dd5e167d10389b6d6153ba87308
cycaub <julien.ubeda31@gmail.com>
2026-02-24 21:20:33 +0100

    erreur variable

--- variables.tf
+++ variables.tf
@@ -3,9 +3,9 @@ variable "vpc_cidr" {
     type = string
   default = "10.0.0.0/16"
 }
-variable "client" {
+variable "entreprise" {
   type        = string
-  description = "Le nom du client passé par Jenkins"
+  description = "Le nom du entreprise passé par Jenkins"
 }
 
 variable "env" {
