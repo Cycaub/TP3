b413bf21b66494a9a7a71a1022a6b4c7fdabb050
cycaub <julien.ubeda31@gmail.com>
2026-02-24 14:21:59 +0100

    rjhpfiaONGVOAZJ

--- variables.tf
+++ variables.tf
@@ -3,3 +3,12 @@ variable "vpc_cidr" {
     type = string
   default = "10.0.0.0/16"
 }
+variable "client" {
+  type        = string
+  description = "Le nom du client passé par Jenkins"
+}
+
+variable "env" {
+  type        = string
+  description = "L'environnement (dev, staging, prod) passé par Jenkins"
+}
